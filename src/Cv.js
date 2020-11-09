0d592c61fb462af32017611c4b17b58b1aa41975
dionisius77 <dionisiusadityaoctanugraha@gmail.com>
2020-11-10 01:50:24 +0700

    finish

--- src/Cv.js
+++ src/Cv.js
@@ -32,23 +32,6 @@ function LinearProgressWithLabel(props) {
 
 
 const useStyles = makeStyles((theme) => ({
-
-  habibi: {
-    marginTop: 20,
-    marginLeft: 30,
-  },
-  roott: {
-    marginLeft: 20
-  },
-  root: {
-    flexGrow: 1,
-  },
-
-  paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
   kiri: {
     backgroundColor: '#000',
     color: 'rgb(247, 186, 221)',
@@ -57,16 +40,15 @@ const useStyles = makeStyles((theme) => ({
   kanan: {
     backgroundColor: 'rgb(247, 186, 221)',
     flex: '30%',
-    padding :-10
   },
   rootGrid: {
     width: '100%',
     margin: 0,
-    padding: -10,
-    display:'flex',
+    padding: 0,
+    display: 'flex',
     flexWrap: 'wrap',
     flexDirection: 'row',
-    height: '100vh',
+    overflowY: 'hidden'
   },
   foto: {
     height: 150,
@@ -88,9 +70,9 @@ const useStyles = makeStyles((theme) => ({
     alingitems: 'center',
   },
   topp: {
+    width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    marginLeft: 22,
   },
   name: {
     margin: 'auto',
@@ -111,7 +93,6 @@ const useStyles = makeStyles((theme) => ({
   anyar: {
     marginTop: 40,
     fontWeight: 700,
-    fontSize: 32,
   },
   display: {
     display: 'flex',
@@ -122,9 +103,9 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: 'Rock Salt,cursive',
     fontSize: 20,
     zIndex: 2,
-    left : 250,
-    top : -117,
-    position : 'relative',
+    marginLeft: 90,
+    top: -117,
+    position: 'relative',
     '&:hover': {
       color: 'rgb(27, 0, 180)',
       textDecoration: 'underline',
@@ -132,14 +113,19 @@ const useStyles = makeStyles((theme) => ({
     }
   },
   habibigruop: {
-    position: 'relative',
+    // position: 'relative',
   },
   imagehabibi: {
     zIndex: 1,
     width: 300,
-    left : 160,
-    top : 20,
-    position : 'relative',
+    marginLeft: 0,
+    top: 20,
+    position: 'relative',
+  },
+  habibiDisplayImage: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 
 }));
@@ -152,131 +138,131 @@ function Cv() {
 
 
   return (
-      <div container className={classes.rootGrid}>
-        <div item xs={8} className={classes.kiri}>
-          <div className={classes.top}>
-            <Avatar alt="logo" src={Lucia} className={classes.large} />
-            <Typography variant="h4" component="h2" className={classes.name} >
-              Mr.Jrin Tea Test
+    <div className={classes.rootGrid}>
+      <div className={classes.kiri}>
+        <div className={classes.top}>
+          <Avatar alt="logo" src={Lucia} className={classes.large} />
+          <Typography variant="h4" className={classes.name} >
+            Mr.Jrin Tea Test
               <br></br>
             Also Known As Diktaktor From EF V AI
           </Typography>
-          </div>
-          <Container>
-            <Typography variant="h4" component="h2" className={classes.profile}>
-              PROFILE
-          </Typography>
-            <br></br>
-            <Typography variant="body1" component="body" className={classes.profilee}>
-              Hai Hai Saya adalah pemuda yang sudah tersesat terlalu jauh dan sudah tidak mungkin untuk diselamatkan
-          </Typography>
-            <Typography variant="h4" component="h2" className={classes.profile}>
-              HERE SOME SPELL MAKE YOUR LIFE BE BETTER :
-          </Typography>
-            <Typography></Typography>
-            <br></br>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography className={classes.heading}>DOA KESELAMATAN</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  “In nomine Dei nostri Satanas Lucifer Excelsa! Dalam nama Setan, penguasa bumi, Raja dari dunia, yang memerintahkan prajurit-prajurit neraka, kami memintamu untuk memberi kuasa kegelapan dalam tangan kami! Buka lebar-lebar gerbang nerakamu dan datanglah dari tempat yang sangat dalam untuk menemui kami sebagai teman dan saudara!”
-          </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography className={classes.heading}>DOA PERMURNIAN</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  API ADALAH NAFAS DARI JIWA, ASAP HITAM ADALAH PEMBEBASAN JIWA, ABU KEMBALI MENJADI ABU, SEMO JIWAMU KEMBALI KE API KEDAMAIAN
-                  LATOM
-          </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion disabled>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3a-content"
-                id="panel3a-header"
-              >
-                <Typography className={classes.heading}>Disabled Accordion</Typography>
-              </AccordionSummary>
-            </Accordion>
-          </Container>
         </div>
-        <div item xs={4} className={classes.kanan}>
-          <div className={classes.topp}>
-            <div className={classes.display}>
-              <EmailIcon style={{ fontsize: 30, marginTop: 47, paddingRight: 10 }} />
-              <Typography variant="h4" component="h2" className={classes.anyar}>
-                OurSaviour26@gmail.com
+        <Container>
+          <Typography variant="h4" className={classes.profile}>
+            PROFILE
+          </Typography>
+          <br></br>
+          <Typography variant="body1" className={classes.profilee}>
+            Hai Hai Saya adalah pemuda yang sudah tersesat terlalu jauh dan sudah tidak mungkin untuk diselamatkan
+          </Typography>
+          <Typography variant="h4" className={classes.profile}>
+            HERE SOME SPELL MAKE YOUR LIFE BE BETTER :
+          </Typography>
+          <br></br>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography className={classes.heading}>DOA KESELAMATAN</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                “In nomine Dei nostri Satanas Lucifer Excelsa! Dalam nama Setan, penguasa bumi, Raja dari dunia, yang memerintahkan prajurit-prajurit neraka, kami memintamu untuk memberi kuasa kegelapan dalam tangan kami! Buka lebar-lebar gerbang nerakamu dan datanglah dari tempat yang sangat dalam untuk menemui kami sebagai teman dan saudara!”
+          </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography className={classes.heading}>DOA PERMURNIAN</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                API ADALAH NAFAS DARI JIWA, ASAP HITAM ADALAH PEMBEBASAN JIWA, ABU KEMBALI MENJADI ABU, SEMO JIWAMU KEMBALI KE API KEDAMAIAN
+                LATOM
+          </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion disabled>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <Typography className={classes.heading}>Disabled Accordion</Typography>
+            </AccordionSummary>
+          </Accordion>
+        </Container>
+      </div>
+      <div className={classes.kanan}>
+        <div className={classes.topp}>
+          <div className={classes.display}>
+            <EmailIcon style={{ fontsize: 30, marginTop: 47, paddingRight: 10 }} />
+            <Typography variant="h5" className={classes.anyar}>
+              OurSaviour26@gmail.com
             </Typography>
-            </div>
-            <div className={classes.display}>
-              <PhoneIcon style={{ fontsize: 30, marginTop: 47, paddingRight: 10 }} />
-              <Typography variant="h4" component="h2" className={classes.anyar}>
-                +62899977666
+          </div>
+          <div className={classes.display}>
+            <PhoneIcon style={{ fontsize: 30, marginTop: 47, paddingRight: 10 }} />
+            <Typography variant="h5" className={classes.anyar}>
+              +62899977666
             </Typography>
-            </div>
-            <div className={classes.habibigroup}>
-              <img alt="habibi" src={habibi} className={classes.imagehabibi}   />
+          </div>
+          <div className={classes.habibiDisplayImage}>
+            <div className={classes.habibigruop}>
+              <img alt="habibi" src={habibi} className={classes.imagehabibi} />
               <Typography onClick={() => { window.open('https://www.instagram.com/habibrizieqlover/') }} className={classes.linkfont}  >
                 OUR KING
             </Typography>
             </div>
           </div>
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6" className={classes.profile}>
+            MY EXPERIENCE
+            </Typography>
           <br></br>
-          <div className={classes.roott}>
-          <Typography variant="h8" component="h2" className={classes.profile}>
-              MY EXPERIENCE
+          <Typography variant="h6">
+            Five Finger
             </Typography>
-            <br></br>
-            <Typography variant="h8" component="h2">
-              Five Finger
+          <LinearProgressWithLabel value={99} />
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6">
+            Sarcasm
             </Typography>
-            <LinearProgressWithLabel value={99} />
-          </div>
-          <div className={classes.roott}>
-            <Typography variant="h8" component="h2">
-              Sarcasm
+          <LinearProgressWithLabel value={66} />
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6">
+            Religious
             </Typography>
-            <LinearProgressWithLabel value={66} />
-          </div>
-          <div className={classes.roott}>
-            <Typography variant="h8" component="h2">
-              Religious
+          <LinearProgressWithLabel value={2} />
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6">
+            Pemuda Tersesat
             </Typography>
-            <LinearProgressWithLabel value={2} />
-          </div>
-          <div className={classes.roott}>
-            <Typography variant="h8" component="h2">
-              Pemuda Tersesat
+          <LinearProgressWithLabel value={86} />
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6">
+            Racism
             </Typography>
-            <LinearProgressWithLabel value={86} />
-          </div>
-          <div className={classes.roott}>
-            <Typography variant="h8" component="h2">
-              Racism
+          <LinearProgressWithLabel value={96} />
+        </div>
+        <div className={classes.roott}>
+          <Typography variant="h6">
+            Gemah
             </Typography>
-            <LinearProgressWithLabel value={96} />
-          </div>
-          <div className={classes.roott}>
-            <Typography variant="h8" component="h2">
-              Gemah
-            </Typography>
-            <LinearProgressWithLabel value={100} />
-          </div>
+          <LinearProgressWithLabel value={100} />
+        </div>
       </div>
     </div>
   );
